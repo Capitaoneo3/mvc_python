0a17cc748d22af02fadae9c7903cf6ed76c0efb3
Capitaoneo3 <joaocapitaneo@gmail.com>
2024-11-11 14:45:47 -0300

    feito tela de update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from controller.usuario_controller import UsuarioController
 from controller.usuario_delete_controller import Usuario_delete_controller
 from model.usuario_model import UsuarioModel
 from view.usuario_delete_view import Usuario_delete_view
+from view.usuario_update_view import Usuario_update_view
 from view.usuario_view import UsuarioView
 
 
@@ -13,8 +14,8 @@ if __name__ == "__main__":
     root.geometry("400x300")
 
     model = UsuarioModel()
-    view = Usuario_delete_view(root)
-    controller = Usuario_delete_controller(view, model)
+    view = Usuario_update_view(root)
+    #controller = Usuario_delete_controller(view, model)
 
     root.mainloop()
     model.fechar_conexao()
